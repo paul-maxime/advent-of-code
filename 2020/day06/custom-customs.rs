288e8e49acb42a079beb4f1524909223795c8e16
Paul-Maxime <github@paulmaxime.fr>
2020-12-06 16:52:29 +0100

    Use a HashSet instead of deduping a Vec

--- 2020/day06/custom-customs.rs
+++ 2020/day06/custom-customs.rs
@@ -1,21 +1,20 @@
 // Day 6: Custom Customs
 
+use std::collections::HashSet;
+
 fn main() {
     let input = std::fs::read_to_string("./input").unwrap();
     let groups: Vec<&str> = input.split("\n\n").collect();
 
     println!("All unique answers: {}", groups.iter().map(|x| get_unique_answers(x).len()).sum::<usize>());
-    println!("Answers with unanimity: {}", groups.iter().map(|x| get_unanimity_answers(x)).sum::<usize>());
+    println!("Unanimous answers: {}", groups.iter().map(|x| get_unanimous_answers(x)).sum::<usize>());
 }
 
-fn get_unique_answers(group: &str) -> Vec<char> {
-    let mut answers: Vec<char> = group.chars().filter(|&c| c.is_alphabetic()).collect();
-    answers.sort();
-    answers.dedup();
-    answers
+fn get_unique_answers(group: &str) -> HashSet<char> {
+    group.chars().filter(|&c| c.is_alphabetic()).collect()
 }
 
-fn get_unanimity_answers(group: &str) -> usize {
+fn get_unanimous_answers(group: &str) -> usize {
     let people: Vec<&str> = group.lines().collect();
     get_unique_answers(group).iter().filter(|&&c| people.iter().all(|&p| p.contains(c))).count()
 }
