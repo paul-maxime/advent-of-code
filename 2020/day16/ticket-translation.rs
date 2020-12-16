084d25225e6d65ea23036406ad9ff26041006d98
Paul-Maxime <github@paulmaxime.fr>
2020-12-16 07:18:08 +0100

    Add day16

---
+++ 2020/day16/ticket-translation.rs
@@ -0,0 +1,87 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+#[derive(Debug)]
+struct Field {
+    name: String,
+    range1: (i32, i32),
+    range2: (i32, i32),
+}
+
+fn main() {
+    let input = std::fs::read_to_string("./input").unwrap();
+
+    let fields = parse_fields(&input);
+    let tickets = parse_tickets(&input);
+
+    println!("Ticket scanning error rate: {}", compute_error_rate(&fields, &tickets));
+    println!("Departure product: {}", compute_departure_product(&fields, &tickets));
+}
+
+// Parsing
+
+fn parse_fields(input: &str) -> Vec<Field> {
+    let field_regex = regex::Regex::new(r"([a-z ]+): (\d+)-(\d+) or (\d+)-(\d+)").unwrap();
+
+    field_regex.captures_iter(input).map(|cap| Field {
+        name: cap[1].to_string(),
+        range1: (cap[2].parse().unwrap(), cap[3].parse().unwrap()),
+        range2: (cap[4].parse().unwrap(), cap[5].parse().unwrap()),
+    }).collect()
+}
+
+fn parse_tickets(input: &str) -> Vec<Vec<i32>> {
+    input.lines()
+        .filter(|x| x.contains(","))
+        .map(|line| line.split(",").map(|x| x.parse().unwrap()).collect())
+        .collect()
+}
+
+// Part 1
+
+fn compute_error_rate(fields: &Vec<Field>, tickets: &Vec<Vec<i32>>) -> i32 {
+    tickets[1..].iter().map(|ticket| ticket.iter().filter(|&&x| !is_valid_field(fields, x)).sum::<i32>()).sum()
+}
+
+fn is_valid_field(fields: &Vec<Field>, value: i32) -> bool {
+    fields.iter().any(|field| is_in_field_range(field, value))
+}
+
+fn is_in_field_range(field: &Field, value: i32) -> bool {
+    field.range1.0 <= value && value <= field.range1.1 || field.range2.0 <= value && value <= field.range2.1
+}
+
+// Part 2
+
+fn compute_departure_product(fields: &Vec<Field>, tickets: &Vec<Vec<i32>>) -> i64 {
+    let mapped_fields = map_index_to_fields(&fields, &tickets);
+    mapped_fields.iter().filter(|x| x.1.starts_with("departure")).map(|x| tickets[0][*x.0]).fold(1, |cur, arr| cur as i64 * arr as i64)
+}
+
+fn map_index_to_fields(fields: &Vec<Field>, tickets: &Vec<Vec<i32>>) -> HashMap<usize, String> {
+    let mut mapped_fields: HashMap<usize, String> = HashMap::new();
+    let mut fields_found: HashSet<String> = HashSet::new();
+
+    let valid_tickets: Vec<&Vec<i32>> = tickets.iter().filter(|&x| is_valid_ticket(fields, x)).collect();
+
+    while fields_found.len() != fields.len() {
+        for i in 0..valid_tickets[0].len() {
+            let field_values: Vec<i32> = valid_tickets.iter().map(|x| x[i]).collect();
+            let matching_fields: Vec<&Field> = fields.iter().filter(|field|
+                !fields_found.contains(&field.name) &&
+                field_values.iter().all(|&x| is_in_field_range(field, x))
+            ).collect();
+
+            if matching_fields.len() == 1 {
+                mapped_fields.insert(i, matching_fields[0].name.clone());
+                fields_found.insert(matching_fields[0].name.clone());
+            }
+        }
+    }
+
+    mapped_fields
+}
+
+fn is_valid_ticket(fields: &Vec<Field>, ticket: &Vec<i32>) -> bool {
+    ticket.iter().all(|&x| is_valid_field(fields, x))
+}
