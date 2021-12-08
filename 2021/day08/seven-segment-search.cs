678a1a43e6f19a5a7b9c833717b4cb3996d3f7e3
Paul-Maxime <github@paulmaxime.fr>
2021-12-08 10:04:54 +0100

    Refactor the day08 to be fully functional

--- 2021/day08/seven-segment-search.cs
+++ 2021/day08/seven-segment-search.cs
@@ -21,46 +21,30 @@ Dictionary<string, int> DIGITS = new Dictionary<string, int> {
     { "abcdef", 9 },
 };
 
-int CountEasyDigits(string[][] input)
-{
-    return input
-        .SelectMany(x => x.TakeLast(4))
-        .Where(x => x.Length == 2 || x.Length == 3 || x.Length == 4 || x.Length == 7)
-        .Count();
-}
+int CountEasyDigits(string[][] input) => input
+    .SelectMany(x => x.TakeLast(4))
+    .Where(x => x.Length == 2 || x.Length == 3 || x.Length == 4 || x.Length == 7)
+    .Count();
 
-static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
-{
-    if (length == 1) return list.Select(t => new T[] { t });
+static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length) =>
+    length == 1 ?
+        list.Select(t => new T[] { t }) :
+        GetPermutations(list, length - 1)
+            .SelectMany(t => list.Where(e => !t.Contains(e)), (t1, t2) => t1.Concat(new T[] { t2 }));
 
-    return GetPermutations(list, length - 1)
-        .SelectMany(t => list.Where(e => !t.Contains(e)), (t1, t2) => t1.Concat(new T[] { t2 }));
-}
+string PermutateString(string input, string to) =>
+    string.Join("", input.Select(x => WIRES.IndexOf(x)).Select(x => to[x]).OrderBy(x => x));
 
-string PermutateString(string input, string to)
-{
-    return string.Join("", input.Select(x => WIRES.IndexOf(x)).Select(x => to[x]).OrderBy(x => x));
-}
+bool IsInputValid(string[] input, string permutation) => input
+    .Select(x => PermutateString(x, permutation))
+    .All(x => DIGITS.ContainsKey(x));
 
-bool IsInputValid(string[] input, string permutation)
-{
-  return input.Select(x => PermutateString(x, permutation)).All(x => DIGITS.ContainsKey(x));
-}
-
-int SumAllOutputs(string[][] input)
-{
-    int sum = 0;
-    foreach (string permutation in GetPermutations(WIRES, WIRES.Length).Select(x => string.Join("", x)))
-    {
-        foreach (var line in input.Where(x => IsInputValid(x, permutation)))
-        {
-            sum += int.Parse(string.Join("",
-                line.TakeLast(4).Select(x => PermutateString(x, permutation)).Select(x => DIGITS[x].ToString())
-            ));
-        }
-    }
-    return sum;
-}
+int SumAllOutputs(string[][] input) => GetPermutations(WIRES, WIRES.Length)
+    .Select(x => string.Join("", x))
+    .SelectMany(permutation => input.Where(x => IsInputValid(x, permutation)).Select(line => (permutation, line)))
+    .Select(result => int.Parse(string.Join("",
+        result.line.TakeLast(4).Select(x => PermutateString(x, result.permutation)).Select(x => DIGITS[x].ToString())
+    ))).Sum();
 
 Console.WriteLine(CountEasyDigits(input));
 Console.WriteLine(SumAllOutputs(input));
