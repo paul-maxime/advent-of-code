f7062cb842ee8984f3ee204710caf3a2545ab8f9
Paul-Maxime <github@paulmaxime.fr>
2021-12-25 06:27:24 +0100

    Add day25 (sea cucumber)

---
+++ 2021/day25/SeaCucumber.cs
@@ -0,0 +1,46 @@
+﻿char[][] map = File.ReadAllLines("input")
+    .Select(line => line.ToCharArray())
+    .ToArray();
+
+bool hasMoved;
+int steps = 0;
+
+do
+{
+    hasMoved = false;
+    steps += 1;
+
+    char[][] copy = map.Select(x => x.ToArray()).ToArray();
+
+    for (int x = 0; x < map[0].Length; x++)
+    {
+        for (int y = 0; y < map.Length; y++)
+        {
+            int toX = (x + 1) % map[0].Length;
+            if (copy[y][x] == '>' && copy[y][toX] == '.')
+            {
+                hasMoved = true;
+                map[y][x] = '.';
+                map[y][toX] = '>';
+            }
+        }
+    }
+
+    copy = map.Select(x => x.ToArray()).ToArray();
+
+    for (int x = 0; x < map[0].Length; x++)
+    {
+        for (int y = 0; y < map.Length; y++)
+        {
+            int toY = (y + 1) % map.Length;
+            if (copy[y][x] == 'v' && copy[toY][x] == '.')
+            {
+                hasMoved = true;
+                map[y][x] = '.';
+                map[toY][x] = 'v';
+            }
+        }
+    }
+} while (hasMoved);
+
+Console.WriteLine(steps);
