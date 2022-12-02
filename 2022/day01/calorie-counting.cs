39e70cac22104ce672b219b606b2bd5373875448
Paul-Maxime <github@paulmaxime.fr>
2022-12-02 22:15:45 +0100

    Add day01 (calorie counting)

---
+++ 2022/day01/calorie-counting.cs
@@ -0,0 +1,7 @@
+﻿int[][] calories = File.ReadAllText("input")
+    .Split("\n\n")
+    .Select(x => x.Split("\n").Where(x => x.Length > 0).Select(x => int.Parse(x)).ToArray())
+    .ToArray();
+
+Console.WriteLine(calories.Select(x => x.Sum()).Max());
+Console.WriteLine(calories.Select(x => x.Sum()).OrderDescending().Take(3).Sum());
