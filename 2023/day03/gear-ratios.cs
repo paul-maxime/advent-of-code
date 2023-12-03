71495b51bbb518300d77b638ab45dc17c4626192
Paul-Maxime <github@paulmaxime.fr>
2023-12-03 13:26:41 +0100

    Clean day03 a little bit

--- 2023/day03/gear-ratios.cs
+++ 2023/day03/gear-ratios.cs
@@ -1,20 +1,20 @@
 var lines = File.ReadAllLines("input");
 
 System.Text.RegularExpressions.Regex numberRegex = new(@"[0-9]+");
-System.Text.RegularExpressions.Regex symbolRegex = new(@"[^.0-9\n]");
+System.Text.RegularExpressions.Regex symbolRegex = new(@"[^.0-9]");
 
 var numbers = lines.SelectMany((line, y) => numberRegex.Matches(line).Select(match => (
     value: long.Parse(match.Value),
     from: match.Index,
     to: match.Index + match.Length - 1,
     y
-)).ToList()).ToList();
+))).ToList();
 
 var symbols = lines.SelectMany((line, y) => symbolRegex.Matches(line).Select(match => (
     type: match.Value,
     x: match.Index,
     y
-)).ToList()).ToList();
+))).ToList();
 
 static bool IsAdjacent((long value, int from, int to, int y) number, (string type, int x, int y) symbol) =>
     (number.y == symbol.y - 1 || number.y == symbol.y || number.y == symbol.y + 1) && // correct y
