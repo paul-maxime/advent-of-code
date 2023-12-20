f6742ee8fbd03106c85f0270299775b54e1ca798
Paul-Maxime <github@paulmaxime.fr>
2023-12-20 01:10:29 +0100

    Add day19 (aplenty), part 2

--- 2023/day19/aplenty.cs
+++ 2023/day19/aplenty.cs
@@ -57,4 +57,47 @@ bool ExecuteWorkflow(string name, long[] part)
     throw new Exception("Unfinished workflow");
 }
 
+long ComputePossibilities(string name, (long min, long max)[] possibilities)
+{
+    if (name == "A")
+    {
+        return possibilities.Select(x => Math.Max(x.max - x.min + 1, 0)).Aggregate((a, b) => a * b);
+    }
+    if (name == "R")
+    {
+        return 0;
+    }
+
+    long total = 0;
+    foreach (string[] instruction in workflows[name])
+    {
+        if (instruction.Length == 2)
+        {
+            string[] operands = instruction[0].Split('>', '<');
+            bool isGreater = instruction[0].Contains('>');
+            long leftIndex = CATEGORIES.IndexOf(operands[0]);
+            long right = long.Parse(operands[1]);
+
+            (long min, long max)[] cloned = [.. possibilities];
+            if (isGreater)
+            {
+                cloned[leftIndex].min = Math.Max(cloned[leftIndex].min, right + 1);
+                possibilities[leftIndex].max = Math.Min(possibilities[leftIndex].max, right);
+            }
+            else
+            {
+                cloned[leftIndex].max = Math.Min(cloned[leftIndex].max, right - 1);
+                possibilities[leftIndex].min = Math.Max(possibilities[leftIndex].min, right);
+            }
+            total += ComputePossibilities(instruction[1], [.. cloned]);
+        }
+        if (instruction.Length == 1)
+        {
+            total += ComputePossibilities(instruction[0], [.. possibilities]);
+        }
+    }
+    return total;
+}
+
 Console.WriteLine(parts.Where(part => ExecuteWorkflow("in", part)).Select(part => part.Sum()).Sum());
+Console.WriteLine(ComputePossibilities("in", [(1, 4000), (1, 4000), (1, 4000), (1, 4000)]));
