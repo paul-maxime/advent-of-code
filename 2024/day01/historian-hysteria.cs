3c4c66a859824d4217f8a17cf8a7032f48559edb
Paul-Maxime <github@paulmaxime.fr>
2024-12-01 06:53:27 +0100

    Add day01 (historian hysteria)

---
+++ 2024/day01/historian-hysteria.cs
@@ -0,0 +1,17 @@
+﻿List<(long left, long right)> lines = File.ReadAllLines("input")
+    .Select(line => line.Split("   "))
+    .Select(data => (left: long.Parse(data[0]), right: long.Parse(data[1])))
+    .ToList();
+
+List<long> leftColumn = lines.Select(x => x.left).Order().ToList();
+List<long> rightColumn = lines.Select(x => x.right).Order().ToList();
+
+long distanceScore = Enumerable.Range(0, lines.Count)
+    .Select(i => Math.Abs(leftColumn[i] - rightColumn[i]))
+    .Sum();
+Console.WriteLine(distanceScore);
+
+long similarityScore = leftColumn
+    .Select(left => left * rightColumn.Count(right => left == right))
+    .Sum();
+Console.WriteLine(similarityScore);
