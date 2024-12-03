de265ffb8017c395b8f329a422cef9584f72c8d9
Paul-Maxime <github@paulmaxime.fr>
2024-12-03 07:45:10 +0100

    Add day03 (mull-it-over)

---
+++ 2024/day03/mull-it-over.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+string input = File.ReadAllText("input");
+
+List<(int index, long result)> mulInstructions = Regex.Matches(input, @"mul\((\d+),(\d+)\)")
+    .Select(match => (
+        index: match.Index,
+        left: long.Parse(match.Groups[1].Value),
+        right: long.Parse(match.Groups[2].Value)
+    ))
+    .Select(mul => (mul.index, mul.left * mul.right))
+    .ToList();
+
+List<(int index, bool canDo)> doInstructions = Regex.Matches(input, @"do\(\)|don't\(\)")
+    .Select(match => (
+        index: match.Index,
+        canDo: match.Value == "do()"
+    ))
+    .Reverse()
+    .Concat([(0, true)])
+    .ToList();
+
+bool CanExecute(int index) => doInstructions.First(x => x.index < index).canDo;
+
+Console.WriteLine(mulInstructions.Select(mul => mul.result).Sum());
+Console.WriteLine(mulInstructions.Where(mul => CanExecute(mul.index)).Select(mul => mul.result).Sum());
