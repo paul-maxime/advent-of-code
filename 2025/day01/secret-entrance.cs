bf9e049b7a178c8153b9683cb0d3e8006456c431
Paul-Maxime <github@paulmaxime.fr>
2025-12-02 03:54:17 +0100

    Add day01 (secret entrance)

---
+++ 2025/day01/secret-entrance.cs
@@ -0,0 +1,36 @@
+﻿int[] input = File.ReadAllLines("input")
+    .Select(line => int.Parse(line[1..]) * (line[0] == 'R' ? 1 : -1))
+    .ToArray();
+
+int ComputeZeros()
+{
+    int dial = 50;
+    int zeros = 0;
+
+    foreach (int rotation in input)
+    {
+        dial = (dial + rotation) % 100;
+        if (dial == 0) zeros += 1;
+    }
+    return zeros;
+}
+
+int ComputeAllZeros()
+{
+    int dial = 50;
+    int zeros = 0;
+
+    foreach (int rotation in input)
+    {
+        for (int i = 0; i < Math.Abs(rotation); i++)
+        {
+            dial = (dial + Math.Sign(rotation)) % 100;
+            if (dial == 0) zeros += 1;
+        }
+    }
+    return zeros;
+}
+
+
+Console.WriteLine(ComputeZeros());
+Console.WriteLine(ComputeAllZeros());
