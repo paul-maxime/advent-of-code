e800cef764bae13b36a1fe1af8ce67c822382a68
Paul-Maxime <github@paulmaxime.fr>
2025-12-03 21:12:54 +0100

    Add day3 (lobby)

---
+++ 2025/day03/lobby.cs
@@ -0,0 +1,12 @@
+﻿string[] input = File.ReadAllLines("input");
+
+string FindLargestBatteries(string bank, int remaining)
+{
+    if (remaining == 0) return "";
+    char first = bank[..^(remaining - 1)].Max();
+    int firstIndex = bank.IndexOf(first);
+    return first + FindLargestBatteries(bank[(firstIndex + 1)..], remaining - 1);
+}
+
+Console.WriteLine(input.Select(x => FindLargestBatteries(x, 2)).Select(long.Parse).Sum());
+Console.WriteLine(input.Select(x => FindLargestBatteries(x, 12)).Select(long.Parse).Sum());
