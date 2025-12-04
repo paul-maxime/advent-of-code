0c6b7022a1bbf68a7d2b111e8a8dbc26e573cb43
Paul-Maxime <github@paulmaxime.fr>
2025-12-04 07:13:51 +0100

    Add day04 (printing department)

---
+++ 2025/day04/printing-department.cs
@@ -0,0 +1,38 @@
+﻿HashSet<(int x, int y)> map = File.ReadAllLines("input")
+    .SelectMany((line, y) => line.Select((c, x) => (x, y, c)))
+    .Where(cell => cell.c == '@')
+    .Select(cell => (cell.x, cell.y))
+    .ToHashSet();
+
+int CountAdjacentRolls(int x, int y)
+{
+    int count = 0;
+    if (map.Contains((x - 1, y - 1))) count++;
+    if (map.Contains((x - 1, y))) count++;
+    if (map.Contains((x - 1, y + 1))) count++;
+    if (map.Contains((x, y - 1))) count++;
+    if (map.Contains((x, y + 1))) count++;
+    if (map.Contains((x + 1, y - 1))) count++;
+    if (map.Contains((x + 1, y))) count++;
+    if (map.Contains((x + 1, y + 1))) count++;
+    return count;
+}
+
+List<(int x, int y)> ReachableRolls()
+{
+    return map.Where(cell => CountAdjacentRolls(cell.x, cell.y) < 4).ToList();
+}
+
+bool RemoveRemoveReachableRolls()
+{
+    var rolls = ReachableRolls();
+    map.RemoveWhere(rolls.Contains);
+    return rolls.Count > 0;
+}
+
+Console.WriteLine(ReachableRolls().Count);
+
+int rollsBefore = map.Count;
+while (RemoveRemoveReachableRolls()) {}
+int rollsAfter = map.Count;
+Console.WriteLine(rollsBefore - rollsAfter);
